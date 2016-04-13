d6eb4b8cc6a5bc9b48f58a9d7992fdb03b0cc204
root <root@ces>
2016-04-13 12:35:28 +0000

    ported scm startup.sh to bash -e

--- containers/scm/startup.sh
+++ containers/scm/startup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 source /etc/ces/functions.sh
 
 # Installuser and should be generated
@@ -7,7 +7,6 @@ ADMUSR="scmadmin"
 ADMPW="scmadmin"
 ADMINGROUP=$(get_config admin_group)
 AUTOUPDATE="1"
-CURLPARAM="-s"
 MAILFROM="cloudoguscm@cloudogu.com"
 RELAYHOST="postfix"
 EXTRAPLUGINS=""
@@ -16,30 +15,19 @@ EXTRAPLUGINS=""
 create_truststore.sh /opt/scm-server/conf/truststore.jks > /dev/null
 
 # Install Redmine plugin if Redmine has started already
-REDMINEKEY=$(etcdctl --peers $(cat /etc/ces/ip_addr):4001 ls /dogu/redmine |wc -l)
+REDMINEKEY=$(etcdctl --peers $(cat /etc/ces/node_master):4001 ls /dogu/redmine |wc -l)
 if [ "$REDMINEKEY" == "1" ]; then
 	EXTRAPLUGINS=$EXTRAPLUGINS$'\nscm-redmine-plugin'
 fi
 
 /opt/scm-server/bin/scm-server &
-tries=0
-while ! [ $(curl "$CURLPARAM" -L -w "%{http_code}" "http://localhost:8080/scm/api/rest/plugins/overview.json" -u "$ADMUSR":"$ADMPW" -o /dev/null) -eq 200 ]
-do
-	((tries++))
-	echo "wait for scm"
-	sleep 1
-	if [ $tries -gt 200 ]
-		then
-			echo "scm didnt start"
-			echo "exit now"
-			kill $!
-			exit 1
-	fi
-done
+echo "INFO: Waiting for scm-manager app to show up .."
+wget -q -o /dev/null -t 20 --timeout=10 --connect-timeout=10 --read-timeout=10 --retry-connrefused --waitretry=10 "http://localhost:8080/scm/" || kill $!
+
 # Plugin installation start
 # get plugin state (json) and list of installed and available plugins
-pluginState=$(/usr/bin/curl "$CURLPARAM" "http://localhost:8080/scm/api/rest/plugins/overview.json" -u "$ADMUSR":"$ADMPW")
-pluginAvail=$(/usr/bin/curl "$CURLPARAM" "http://localhost:8080/scm/api/rest/plugins/available.json" -u "$ADMUSR":"$ADMPW")
+pluginState=$(/usr/bin/curl -s "http://localhost:8080/scm/api/rest/plugins/overview.json" -u "$ADMUSR":"$ADMPW")
+pluginAvail=$(/usr/bin/curl -s "http://localhost:8080/scm/api/rest/plugins/available.json" -u "$ADMUSR":"$ADMPW")
 echo "================ plugin installation loop ================"
 for i in $(cat /opt/scm-server/conf/pluginlist)$EXTRAPLUGINS; do
 	echo "================== installing $i plugin =================="
@@ -56,12 +44,12 @@ for i in $(cat /opt/scm-server/conf/pluginlist)$EXTRAPLUGINS; do
 	echo "=========================================================="
 	# PLUGIN NOT INSTALLED --> INSTALL
 	if [ "$state" == "AVAILABLE" ]; then
-		/usr/bin/curl "$CURLPARAM" -X POST "http://localhost:8080/scm/api/rest/plugins/install/$groupId:$i:$version" -u "$ADMUSR":"$ADMPW"
+		/usr/bin/curl -s -X POST "http://localhost:8080/scm/api/rest/plugins/install/$groupId:$i:$version" -u "$ADMUSR":"$ADMPW"
 		echo "===================== installed $i ======================="
 	else
 	# PLUGIN INSTALLED BUT OUTDATED --> UPDATE
 		if ! [ "$version" == "versionAvail" ] || [ "$AUTOUPDATE" == "1" ]; then
-			/usr/bin/curl "$CURLPARAM" -X POST "http://localhost:8080/scm/api/rest/plugins/update/$groupId:$i:$version" -u "$ADMUSR":"$ADMPW"
+			/usr/bin/curl -s -X POST "http://localhost:8080/scm/api/rest/plugins/update/$groupId:$i:$version" -u "$ADMUSR":"$ADMPW"
 			echo "====================== updated $i ========================"
 		fi
 	fi
@@ -74,12 +62,12 @@ fi
 render_template "/opt/scm-server/conf/cas_plugin.xml.tpl" > "/var/lib/scm/config/cas_plugin.xml"
 
 # configure admin group using api rest calls and json
-configState=$(/usr/bin/curl "$CURLPARAM" "http://127.0.0.1:8080/scm/api/rest/config.json" -u "$ADMUSR":"$ADMPW")
+configState=$(/usr/bin/curl -s "http://127.0.0.1:8080/scm/api/rest/config.json" -u "$ADMUSR":"$ADMPW")
 ## add group in case it is missing e.g. "admin-groups": "universalAdmin"
 adminGroups=`echo "$configState" | jq -r '.["admin-groups"]'`
 if [ "$adminGroups" == "null" ]; then
 	newConfigState=$(echo "$configState" | jq ".+= {\"admin-groups\": \"$ADMINGROUP\"}" | jq ".+= {\"base-url\": \"http://$FQDN/scm\"}")
-	curl "$CURLPARAM" -H "Content-Type: application/json" -X POST -d "$newConfigState" "http://127.0.0.1:8080/scm/api/rest/config.json" -u "$ADMUSR":"$ADMPW"
+	curl -s -H "Content-Type: application/json" -X POST -d "$newConfigState" "http://127.0.0.1:8080/scm/api/rest/config.json" -u "$ADMUSR":"$ADMPW"
 fi
 
 # configure scm-mail-plugin
