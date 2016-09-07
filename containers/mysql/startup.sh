5b95625c6e2107eb6b25ba73295c2f83192705f5
Robert Auer <robert.auer@cloudogu.com>
2016-09-07 08:48:08 +0200

    Changed mysql dogu to manual build of MySQL 5.7.15 (not successful); #197

--- containers/mysql/startup.sh
+++ containers/mysql/startup.sh
@@ -3,9 +3,38 @@ mkdir -p /var/run/mysqld
 chown -R mysql: /var/run/mysqld
 
 # installation
-if [ ! -f /var/lib/mysql/ibdata1 ]; then
+#if [ ! -f /var/lib/mysql/ibdata1 ]; then
+if [ ! -d /usr/local/mysql ]; then
     # set stage for health check
     doguctl state installing
+
+    echo "Starting steps originally done in Dockerfile..."
+    mv /usr/local/mysql-5.7.14-linux-glibc2.5-x86_64 /usr/local/mysql
+    #RUN cd /usr/local/mysql
+    mkdir /usr/local/mysql/mysql-files
+    chmod 750 /usr/local/mysql/mysql-files
+    chown -R mysql /usr/local/mysql
+    chgrp -R mysql /usr/local/mysql
+
+    apk add --update libstdc++ libgcc
+    ldd --version
+
+    # last part from http://dev.mysql.com/doc/refman/5.7/en/binary-installation.html
+    echo "Starting installation steps in startup.sh now"
+    su -c "/usr/local/mysql/bin/mysqld --initialize --user=mysql" mysql
+    # MySQL 5.7.6 and up
+    ls -l /usr/local/mysql/bin/
+    ldd /usr/local/mysql/bin/mysql_ssl_rsa_setup
+    /usr/local/mysql/bin/mysql_ssl_rsa_setup
+    chown -R root /usr/local/mysql
+    chown -R mysql /usr/local/mysql/data /usr/local/mysql/mysql-files
+    /usr/local/mysql/bin/mysqld_safe --user=mysql &
+
+    # bind server to 0.0.0.0
+    echo "sed command now"
+    sed -i -e"s/^bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
+
+
 
     # install database
     mysql_install_db --user=mysql --datadir="/var/lib/mysql"
@@ -33,13 +62,13 @@ if [ ! -f /var/lib/mysql/ibdata1 ]; then
 
     # set stage for health check
     doguctl state ready
-
+echo "ready now"
     # reattach daemon
     wait
 else
   # set stage for health check
   doguctl state ready
-
+echo "ready now"
   # start mysql
   exec /usr/bin/mysqld_safe --user=mysql
 fi
